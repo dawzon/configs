993f7acbcbb2973e5eddf504c8d76df38c666331
Dawson Coleman <dawsonmcoleman@gmail.com>
2026-02-01 19:57:41 -0600

    nvim: Add oxocarbon colorscheme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,13 @@
 return {
-	"webhooked/kanso.nvim",
-	lazy = false,
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme("kanso-zen")
-	end,
+	-- Default colorscheme
+	{
+		"webhooked/kanso.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.cmd.colorscheme("kanso-zen")
+		end,
+	},
+	-- Extras
+	"nyoom-engineering/oxocarbon.nvim",
 }
